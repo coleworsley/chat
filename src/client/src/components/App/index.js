4ff28cae123cd001d462b5b63c8d04e0f01b72e7
coleworsley <cole.s.worsley@gmail.com>
2019-10-31 00:35:23 -0600

    refactor App component to include Messages

--- src/client/src/components/App/index.js
+++ src/client/src/components/App/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import io from 'socket.io-client';
 import './index.css';
+import { SocketContext } from '../../context';
+import { Messages } from '../Messages';
 
-function App() {
+const HOST = 'http://localhost:8080';
+const socket = io(HOST);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the <em>CHAT</em></h1>
-      </header>
-    </div>
+    <SocketContext.Provider value={({socket})}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to the <em>CHAT</em></h1>
+        </header>
+        <main>
+          <Messages />
+        </main>
+      </div>
+    </SocketContext.Provider>
   );
 }
 
